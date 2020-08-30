c4f4754e23ed74073e2bbff6c2bb758c3d908f09
painan <112233@qq.com>
2020-08-30 10:06:41 +0800

    dev tijiao

--- XX/XX/src/cc/cc/CCC.java
+++ XX/XX/src/cc/cc/CCC.java
@@ -5,13 +5,5 @@ public class CCC {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("少时诵诗书所"); //dev分支 ??
-		
-		
-		
-		
-		
-		// XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXx
-		// DEV DEVDEV
 	}
-
 }
